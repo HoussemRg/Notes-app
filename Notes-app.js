0b6e0be187576a1fc6574132ddc97f1f7733c0e9
houssem <houssem.regaieg@etudiant-enit.utm.tn>
2023-06-18 14:06:25 +0100

    last version

--- Notes-app.js
+++ Notes-app.js
@@ -1,20 +1,52 @@
-
-let arrayOfNotes=new Array();
 let secondContainer=document.querySelector('.flex-2');
+let savedContent=localStorage.getItem('flex2');
+if(!savedContent){
+    secondContainer.innerHTML='';
+}
+else{
+    secondContainer.innerHTML=savedContent;
+}
+let arrayOfNotes=new Array();
+
 console.log(secondContainer.innerHTML);
 let index=0;
 function addNote(){
     secondContainer.innerHTML+=`<div class="text" id="note">
-                               <textarea name="txt" id="txt" cols="52" rows="13"></textarea>
+                               <textarea name="txt" id="txt-${index}" cols="52" rows="13" oninput="saveContent(${index});"></textarea>
                                <button id="del-btn-${index}" onclick="deleteNote(this);"><img src="images/delete.png" alt="del" class="img-del"></button>
                                </div>`;
+    localStorage.setItem('flex2',secondContainer.innerHTML);
+    index++;
 }
 let btn=document.querySelector('#btn');
 btn.onclick=addNote;
 
 function deleteNote(button){
-    button.parentElement.remove();
+    let div=button.parentElement;
+    let index=div.querySelector('textarea').id.split('-')[1];
+    localStorage.removeItem(`txt-${index}`);
+    console.log(localStorage);
+    div.remove();
+    localStorage.setItem('flex2',secondContainer.innerHTML);
 }
 
+function saveContent(index){
+    let textarea=document.getElementById(`txt-${index}`);
+    let save=textarea.value;
+    localStorage.setItem(`txt-${index}`,save);
+}
+function restoreContent(index){
+    let saved=localStorage.getItem(`txt-${index}`);
+    let textarea=document.getElementById(`txt-${index}`);
+    console.log(saved);
+    if(saved){
+        textarea.value=saved;
+    }  
+}
+for(let i=0;i<index;i++){
+    restoreContent(i);
+}
+
+console.log(localStorage);
 
 
